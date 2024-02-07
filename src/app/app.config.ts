9ed592f448b9f8665595251b16cdfdff139caf29
Helanka Singhapurage <helankas26@gmail.com>
2024-02-07 21:19:55 +0530

    add http interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {routes} from './app.routes';
+import {provideClientHydration} from '@angular/platform-browser';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
+import {httpManagerInterceptor} from "./modules/utility/interceptor/http-manager.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideHttpClient(
+    withInterceptors(
+      [httpManagerInterceptor]
+    )
+  ), provideClientHydration(), provideAnimationsAsync()]
 };
