53ad9777abe104d917b94d52f6d2ee72dcb95c5f
Helanka Singhapurage <helankas26@gmail.com>
2024-02-20 20:06:08 +0530

    add load all product

--- src/app/modules/utility/services/products/products.service.ts
+++ src/app/modules/utility/services/products/products.service.ts
@@ -6,13 +6,14 @@ import {Observable} from "rxjs";
   providedIn: 'root'
 })
 export class ProductsService {
-  url = 'http://localhost:3002/api/v1'
+
+  url = 'http://localhost:3002/api/v1/';
 
   constructor(private http: HttpClient) {
   }
 
   public loadProducts(page: any, size: any, searchText: any): Observable<any> {
-    return this.http.get(this.url);
+    return this.http.get(this.url + 'products/');
   }
 
 }
--- src/app/pages/product/product-page/product-page.component.ts
+++ src/app/pages/product/product-page/product-page.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ProductViewCardComponent} from "../../../shared/cards/product-view-card/product-view-card.component";
 import {RouterLink} from "@angular/router";
+import {ProductsService} from "../../../modules/utility/services/products/products.service";
 
 @Component({
   selector: 'app-product-page',
@@ -12,6 +13,25 @@ import {RouterLink} from "@angular/router";
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.scss'
 })
-export class ProductPageComponent {
+export class ProductPageComponent implements OnInit {
+  page: any = 0;
+  size: any = 10;
+  searchText = ''; // debounce
+
+
+  //s=inject(ProductsService);
+
+  constructor(private productService: ProductsService) {
+  }
+
+  ngOnInit(): void {
+    this.loadAllProducts(this.page, this.size, this.searchText);
+  }
+
+  private loadAllProducts(page: any, size: any, searchText: string) {
+    this.productService.loadProducts(page, size, searchText).subscribe(response => {
+      console.log(response);
+    })
+  }
 
 }
