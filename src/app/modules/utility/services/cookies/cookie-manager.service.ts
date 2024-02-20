0fc90f9674e051b34032fcd1c34ad22b6e02ce36
Helanka Singhapurage <helankas26@gmail.com>
2024-02-20 19:06:38 +0530

    implement cookie service

--- src/app/modules/utility/services/cookies/cookie-manager.service.ts
+++ src/app/modules/utility/services/cookies/cookie-manager.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {CookieService} from "ngx-cookie-service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CookieManagerService {
 
-  constructor() { }
+  constructor(private cookieService: CookieService) {
+  }
+
+  public createToken(value: any) {
+    this.cookieService.set('token', value, 30, '/');
+  }
+
+  public isTokenExists(): boolean {
+    return this.cookieService.check('token');
+  }
+
+  public removeToken() {
+    if (this.isTokenExists()) {
+      this.cookieService.delete('token');
+    }
+  }
 }
